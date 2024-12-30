2d60ebe8411fdae383679fbb09643a5f679c76d1
barama <Josu Barrutia>
2024-12-30 11:45:22 +0100

    Añadir badges

--- src/config/addData.js
+++ src/config/addData.js
@@ -2,6 +2,8 @@
 
 const Skill = require('../models/Skill');
 const skills = require('../public/electronics/skillsLLM.json');
+const badges = require('../public/badges/medallas.json');
+const Badge = require('../models/Badge');
 
 exports.addSkills = async () => {
     try {
@@ -20,5 +22,27 @@ exports.addSkills = async () => {
         console.log('Skills añadidas a la base de datos');
     } catch (error) {
         console.error('Error al añadir skills a la base de datos:', error);
+    }
+}
+
+exports.addBadges = async () => {
+    try {
+        // Eliminar los rangos existentes antes de añadir los nuevos
+        await Badge.deleteMany();
+
+        // Transformar los datos si es necesario, en este caso no es necesario
+        const transformedRanks = badges.map(rango => ({
+            name: rango.rango,
+            bitpoints_min: rango.bitpoints_min,
+            bitpoints_max: rango.bitpoints_max,
+            image_url: rango.png
+        }));
+
+        // Insertar los nuevos rangos en la base de datos
+        await Badge.insertMany(transformedRanks);
+
+        console.log('Rangos añadidos a la base de datos');
+    } catch (error) {
+        console.error('Error al añadir rangos a la base de datos:', error);
     }
 }
